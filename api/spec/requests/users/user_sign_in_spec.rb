4bd22c4e66e5154e9348363acc6cc7b19019f4d5
Gabriel Manika <gmkoeb@gmail.com>
2024-07-09 11:58:33 -0300

    Add authentication via devise-jwt
    - Edited devise default registrations contoller and sessions
    controllers to adapt to the JWT api authentication.
    
    - Due to an issue in Rails 7 API-only app, it was added rack_sessions_fix.rb t
    o create a fake session hash for devise.

---
+++ api/spec/requests/users/user_sign_in_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'User signs in' do
+  it 'successfully' do
+    User.create(name: 'Test', email: 'test@email.com', password: '123456')
+
+    post user_session_path, params: { user: { email: 'test@email.com', password: '123456' } }
+    json_response = JSON.parse(response.body)
+
+    expect(response.status).to eq 200
+    expect(json_response['status']['message']).to eq 'Logged in successfully.'
+    expect(json_response['status']['data']['user']['name']).to eq 'Test'
+    expect(json_response['status']['data']['user']['email']).to eq 'test@email.com'
+  end
+
+  it 'with wrong parameters' do
+    User.create(name: 'Test', email: 'test@email.com', password: '123456')
+
+    post user_session_path, params: { user: { email: 'test@email.com', password: '43214' } }
+
+    expect(response.status).to eq 401
+    expect(response.body).to eq 'Invalid Email or password.'
+  end
+end
---
+++ api/spec/requests/users/user_sign_up_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+describe 'User signs up' do
+  it 'successfully' do
+    post user_registration_path, params: { user: { name: 'Test', email: 'test@email.com', password: '123456' } }
+
+    json_response = JSON.parse(response.body)
+    user = User.last
+
+    expect(response.status).to eq 200
+    expect(json_response['status']['message']).to eq 'Signed up successfully.'
+    expect(json_response['data']['id']).to eq 1
+    expect(json_response['data']['email']).to eq 'test@email.com'
+    expect(json_response['data']['name']).to eq 'Test'
+    expect(user.name).to eq 'Test'
+    expect(user.email).to eq 'test@email.com'
+  end
+
+  it 'with blank parameters' do
+    post user_registration_path, params: { user: { name: '', email: '', password: '' } }
+
+    json_response = JSON.parse(response.body)
+
+    expect(response.status).to eq 422
+    expect(json_response['status']['message']).to eq "User couldn't be created successfully. Email can't be blank, " \
+                                                     "Password can't be blank, and Name can't be blank"
+    expect(User.last).to be_nil
+  end
+
+  it 'with invalid password' do
+    post user_registration_path, params: { user: { name: 'Test', email: 'test2@email.com', password: '12345' } }
+
+    json_response = JSON.parse(response.body)
+
+    expect(response.status).to eq 422
+    expect(json_response['status']['message']).to eq "User couldn't be created successfully. " \
+                                                     'Password is too short (minimum is 6 characters)'
+    expect(User.last).to be_nil
+  end
+
+  it 'with non-unique email' do
+    User.create(name: 'Test', email: 'test@email.com', password: '123456')
+    user = User.last
+    post user_registration_path, params: { user: { name: 'Test 2', email: 'test@email.com', password: '123456' } }
+
+    json_response = JSON.parse(response.body)
+
+    expect(response.status).to eq 422
+    expect(json_response['status']['message']).to eq "User couldn't be created successfully. " \
+                                                     'Email has already been taken'
+    expect(User.last).to eq user
+  end
+
+  it 'with non-unique name' do
+    User.create(name: 'Test', email: 'test@email.com', password: '123456')
+    user = User.last
+    post user_registration_path, params: { user: { name: 'Test', email: 'test2@email.com', password: '123456' } }
+
+    json_response = JSON.parse(response.body)
+
+    expect(response.status).to eq 422
+    expect(json_response['status']['message']).to eq "User couldn't be created successfully. " \
+                                                     'Name has already been taken'
+    expect(User.last).to eq user
+  end
+end
