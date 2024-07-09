261144edd2f8beaeb9b32306ec86c022d634b04b
Gabriel Manika <gmkoeb@gmail.com>
2024-07-09 13:09:00 -0300

    Add axios and js-cookie to handle api requests and cookies

---
+++ web/api/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+import Cookies from 'js-cookie'
+
+export const api = axios.create({
+    baseURL: 'http://localhost:3000'
+})
+
+if (Cookies.get('token')) {
+    api.defaults.headers.common = {
+        'Authorization': `Bearer ${Cookies.get('token')}`
+    }
+}
