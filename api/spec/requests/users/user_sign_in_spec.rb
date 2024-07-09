2e41bf5364c0bde8a364313765e8746bc0bd7836
Gabriel Manika <gmkoeb@gmail.com>
2024-07-09 15:00:54 -0300

    Refactor some tests and fix rubocop offenses

--- api/spec/requests/users/user_sign_in_spec.rb
+++ api/spec/requests/users/user_sign_in_spec.rb
@@ -1,24 +1,26 @@
 require 'rails_helper'
 
 describe 'User signs in' do
-  it 'successfully' do
-    User.create(name: 'Test', email: 'test@email.com', password: '123456')
+  context 'POST /login' do
+    it 'success' do
+      User.create(name: 'Test', email: 'test@email.com', password: '123456')
 
-    post user_session_path, params: { user: { email: 'test@email.com', password: '123456' } }
-    json_response = JSON.parse(response.body)
+      post user_session_path, params: { user: { email: 'test@email.com', password: '123456' } }
+      json_response = JSON.parse(response.body)
 
-    expect(response.status).to eq 200
-    expect(json_response['status']['message']).to eq 'Logged in successfully.'
-    expect(json_response['status']['data']['user']['name']).to eq 'Test'
-    expect(json_response['status']['data']['user']['email']).to eq 'test@email.com'
-  end
+      expect(response.status).to eq 200
+      expect(json_response['status']['message']).to eq 'Logged in successfully.'
+      expect(json_response['status']['data']['user']['name']).to eq 'Test'
+      expect(json_response['status']['data']['user']['email']).to eq 'test@email.com'
+    end
 
-  it 'with wrong parameters' do
-    User.create(name: 'Test', email: 'test@email.com', password: '123456')
+    it 'with wrong parameters' do
+      User.create(name: 'Test', email: 'test@email.com', password: '123456')
 
-    post user_session_path, params: { user: { email: 'test@email.com', password: '43214' } }
+      post user_session_path, params: { user: { email: 'test@email.com', password: '43214' } }
 
-    expect(response.status).to eq 401
-    expect(response.body).to eq 'Invalid Email or password.'
+      expect(response.status).to eq 401
+      expect(response.body).to eq 'Invalid Email or password.'
+    end
   end
 end
--- api/spec/requests/users/user_sign_out_spec.rb
+++ api/spec/requests/users/user_sign_out_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
 describe 'User signs out' do
-  it 'successfully' do
-    User.create(name: 'Test', email: 'test@email.com', password: '123456')
+  context 'DELETE /logout' do
+    it 'successfully' do
+      User.create(name: 'Test', email: 'test@email.com', password: '123456')
 
-    post user_session_path, params: { user: { email: 'test@email.com', password: '123456' } }
-    json_response = JSON.parse(response.body)
-    token = json_response['status']['data']['Authorization']['token']
+      post user_session_path, params: { user: { email: 'test@email.com', password: '123456' } }
+      json_response = JSON.parse(response.body)
+      token = json_response['status']['data']['Authorization']['token']
 
-    delete destroy_user_session_path, headers: { Authorization: token }
-    json_response = JSON.parse(response.body)
+      delete destroy_user_session_path, headers: { Authorization: token }
+      json_response = JSON.parse(response.body)
 
-    expect(response.status).to eq 200
-    expect(json_response['message']).to eq 'Logged out successfully.'
+      expect(response.status).to eq 200
+      expect(json_response['message']).to eq 'Logged out successfully.'
+    end
   end
 end
